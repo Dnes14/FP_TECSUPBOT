4a26aa704b3ed64f4158b2ec4c89b2615aaa704a
Daniel Escriba <danielescriba.f@outlook.com>
2024-01-25 03:11:52 -0500

    prueba reder

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ from flask import Flask, render_template, request
 from chatbot import predict_class,get_response
 import json
 
-intents = json.loads(open('intents.json',encoding='utf-8').read())
+def apprender():
+    intents = json.loads(open('intents.json',encoding='utf-8').read())
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('botchat.html')
+    @app.route("/")
+    def index():
+        return render_template('botchat.html')
 
 
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    ints = predict_class(msg)
-    return get_response(ints, intents)
+    @app.route("/get", methods=["GET", "POST"])
+    def chat():
+        msg = request.form["msg"]
+        ints = predict_class(msg)
+        return get_response(ints, intents)
+    return app
 
 
 
 if __name__ == '__main__':
+    app = apprender()
     app.run()(debug=True)
